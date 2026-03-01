e2bf24e71a03e5185fae87f0b909bcec83601a26
hadam94 <hadam@student.bridgew.edu>
2026-03-01 15:35:44 -0500

    Improve AI Demo test

--- src/speech/test/AIDemoTest.java
+++ src/speech/test/AIDemoTest.java
@@ -9,7 +9,10 @@ import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModelName;
 import speech.AIAgentDemo;
 import speech.booking.RoomBookingRequests;
+import speech.util.JsonObject;
 import speech.util.SpeechListenerUtil;
+import speech.util.TimeUtil;
+
 import static speech.util.PrintUtil.print;;
 
 public class AIDemoTest {
@@ -23,7 +26,24 @@ public class AIDemoTest {
 		ChatModel model = OpenAiChatModel.builder().apiKey(AIAgentDemo.API_KEY).modelName(OpenAiChatModelName.GPT_5).build();
 		RoomBookingRequests booking = new RoomBookingRequests();
 		booking.login("Comp490.002@bridgew.edu", "TuesThurs12:30");
-		String response = model.chat(speech + "? \n\n" + booking.listAvalibleMeetingRooms());
-		print(response);
+		String time = TimeUtil.getCurrentTime();
+		StringBuilder roomsList = new StringBuilder();
+		for(JsonObject object: booking.listAvalibleMeetingRooms()) {
+			roomsList.append(object + ",");
+		}
+		
+		//the list of bookings, massive
+		StringBuilder bookedRooms = new StringBuilder();
+		int i = 0;
+		for(JsonObject object: booking.listMyBookings()) {
+			if(i % 2000 != 0) {
+				bookedRooms.append(object.toString() + "\n");
+			} else {
+				String response = model.chat("Todays time is " + time + ". \n" + "The current rooms are: " + roomsList.toString() + "\n The booked rooms are: " + bookedRooms.toString() + "\n\n " + speech + "?");
+				print(response);
+				bookedRooms = new StringBuilder();
+			}
+			i++;
+		}	
 	}
 }
