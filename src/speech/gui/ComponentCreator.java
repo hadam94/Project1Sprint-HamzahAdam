7deddbd2263694c944a0e7c561188eac4a2c8480
hadam94 <hadam@student.bridgew.edu>
2026-03-16 20:11:11 -0400

    Add support for text fields, and rendering string on screen

---
+++ src/speech/gui/ComponentCreator.java
@@ -0,0 +1,35 @@
+package speech.gui;
+
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.TextField;
+import java.awt.event.ActionListener;
+
+public class ComponentCreator {
+	
+	public static Button addButtonComponent(String name, Point position, Dimension size, ActionListener action) {
+		Button button = new Button();
+		button.setLabel(name);
+		button.setBounds(position.x, position.y, size.width, size.height);
+		if(action != null) {
+			button.addActionListener(action);
+		}
+		button.setVisible(true);
+		GuiWindow.getInstance().add(button);
+		return button;
+	}
+	
+	public static TextField addTextBoxComponent(String name, Point position, Dimension size, ActionListener action) {
+		TextField field = new TextField();
+		field.setName(name);
+		field.setBounds(position.x, position.y, size.width, size.height);
+		if(action != null) {
+			field.addActionListener(action);
+		}
+		field.setVisible(true);
+		GuiWindow.getInstance().add(field);
+		return field;
+	}
+
+}
--- src/speech/gui/GuiWindow.java
+++ src/speech/gui/GuiWindow.java
@@ -1,16 +1,15 @@
 package speech.gui;
 
-import java.awt.Button;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.TextField;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import speech.util.PrintUtil;
 
@@ -19,66 +18,53 @@ public class GuiWindow extends JFrame {
 	
 	private final Dimension windowDimension = new Dimension(1000, 1000);
 	
+	private final TextField roomname;
+	private static GuiWindow instance;
+	
+	int x = 0;
+	
 	public GuiWindow() {
+		instance = this;
 		setResizable(false);
 		setSize(windowDimension);
 		setTitle("Server Test");
-		addWindowListener(new WindowListener() {
-			
-			@Override
-			public void windowOpened(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void windowIconified(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void windowDeiconified(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void windowDeactivated(WindowEvent e) {
-				// TODO Auto-generated method stub
-			}
-			
-			@Override
-			public void windowClosing(WindowEvent e) {
-				// TODO Auto-generated method stub
-				System.exit(-1);
-			}
-			
-			@Override
-			public void windowClosed(WindowEvent e) {
-				// TODO Auto-generated method stub
-			}
-			
-			@Override
-			public void windowActivated(WindowEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		getContentPane().setBackground(Color.GRAY);
-		addButtonComponent("Test", new Point(10, 10), new Dimension(50, 50), (e) -> PrintUtil.print("Hello World"));
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.GRAY);
-		add(panel);
+		addWindowListener(new WindowOnCloseListener());
+		
+		roomname = ComponentCreator.addTextBoxComponent("Room Name", new Point(100, windowDimension.height - 300), new Dimension(100, 20), null);
+		ComponentCreator.addButtonComponent("Add Room", new Point(100, windowDimension.height - 100), new Dimension(100, 50), (e) -> PrintUtil.print("Add Room"));
+		
+		
+		add(new RenderEvent());
+		
 		setVisible(true);
 	}
 	
-	private void addButtonComponent(String name, Point position, Dimension size, ActionListener action) {
-		Button button = new Button();
-		button.setLabel(name);
-		button.setBounds(position.x, position.y, size.width, size.height);
-		button.addActionListener(action);
-		button.setVisible(true);
-		add(button);
+	public static GuiWindow getInstance() {
+		return instance;
+	}
+	
+	public void onRender(Graphics2D graphics) {
+		x++;
+		graphics.drawString("Hello", x, 50);	
+		PrintUtil.print("called " + Math.random());
+	}
+	
+	private class RenderEvent extends JPanel {
+		
+		private final Timer timer = new Timer(16, (action) -> {
+			repaint();
+		});
+		
+		private RenderEvent() {
+			setBackground(Color.GRAY);
+			timer.start();
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			// TODO Auto-generated method stub
+			super.paintComponent(g);
+			onRender((Graphics2D) g);
+		}
 	}
 }
